8d941d82dd638669a8f393948acf41b5d773df72
Kirill <nikitinkirill82@yandex.ru>
2023-07-03 15:27:37 +0300

    Pract06

--- 22_nabor_chisel.py
+++ 22_nabor_chisel.py
@@ -2,12 +2,26 @@
 # Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
 # Пользователь вводит 2 числа. n — кол-во элементов первого множества.
 # m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
-
 from random import randint
 
-my_list_1 = [randint(0, 20) for _ in range(int(input("Введите количество элементов 1: ")))]
-my_list_2 = [randint(0, 20) for _ in range(int(input("Введите количество элементов 2: ")))]
-my_list_3 = set(sorted(my_list_1 + my_list_2))
-print(my_list_1)
-print(my_list_2)
-print(my_list_3)
+numberN = int(input("Введите количество элементов первого множества: "))
+numberM = int(input("Введите количество элементов второго множества: "))
+
+list_1 = [randint(0,9) for _ in range(numberN)]
+list_2 = [randint(0,9) for _ in range(numberM)]
+
+print(list_1)
+print(list_2)
+
+##list_3=[]
+##
+##for i in range(len(list_1)):
+##    for j in range(len(list_2)):
+##        if list_1[i] == list_2[j]:
+##            list_3.append(list_1[i])
+
+list_3 = [elem for elem in list_1 if elem not in list_2]
+
+print(list_3)
+
+##print(sorted(set(list_1)&set(list_2)))
